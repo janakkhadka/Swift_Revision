e8942c7ee085971af769900c6e4cb1078d5f878b
Janak Khadka <janakkhadka08@gmail.com>
2025-02-13 22:56:37 +0545

    function with return value

--- Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
+++ Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
@@ -16,3 +16,12 @@ func myExampleFunction(name: String, roll: Int, age: Int){
 
 //function call garda arguement pass garda kheri colon : use garnu parxa with parameter name
 myExampleFunction(name:"janak",roll:123,age:20)
+
+
+//fucntion with returning value
+func returnMyName() -> String {
+    return "janak"
+}
+
+let name: String = returnMyName()
+print(name)
