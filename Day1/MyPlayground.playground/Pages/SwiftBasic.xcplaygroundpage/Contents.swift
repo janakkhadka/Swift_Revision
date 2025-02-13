4d0a4d0fd9d8ce2bdf64475f780317074ba3a55c
Janak Khadka <janakkhadka08@gmail.com>
2025-02-13 22:31:11 +0545

    loop and conditionals

--- Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
+++ Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 var greeting = "Hello, playground"
-let a: Int = 10
-print(a)
+
+
---
+++ Day1/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+var greeting = "Hello, playground"
+let a: Int = 10
+print(a)
+
+
+//conditionals
+if a > 5 {
+    print("hello guys")
+}else{
+    print("nepali hami")
+}
+
+
+//loop
+var i = 1
+while i <= 5 {
+    print(i)
+    i+=1
+}
