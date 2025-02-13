1548e4191c9c9016e786e1cede03f34323b73cbf
Janak Khadka <janakkhadka08@gmail.com>
2025-02-13 22:53:20 +0545

    function in swift

--- Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
+++ Day1/MyPlayground.playground/Pages/SwiftBasic.xcplaygroundpage/Contents.swift
@@ -1,5 +1,18 @@
 import Foundation
 
-var greeting = "Hello, playground"
+//function
+func myExampleFunction(){
+    print("first function execution!")
+}
 
+myExampleFunction()
 
+//function with parameter
+func myExampleFunction(name: String, roll: Int, age: Int){
+    print("name: ",name)
+    print("roll: ",roll)
+    print("age: ",age)
+}
+
+//function call garda arguement pass garda kheri colon : use garnu parxa with parameter name
+myExampleFunction(name:"janak",roll:123,age:20)
