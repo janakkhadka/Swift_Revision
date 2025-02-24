f4655155a61d31159ddfb35db54638f8a4d65d95
Janak Khadka <janakkhadka08@gmail.com>
2025-02-24 21:25:59 +0545

    array value adding and deleting

--- Day2/Basic.playground/Pages/PlayingWithCollection.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/PlayingWithCollection.xcplaygroundpage/Contents.swift
@@ -4,14 +4,31 @@ import Foundation
 
 //array with default value
 var firstArray = Array(repeating: 1, count: 5)
-print(firstArray)
+//print(firstArray)
 
 var secondArray = Array(repeating: "a", count: 5)
-print(secondArray)
+//print(secondArray)
 
 //var finalArray = firstArray + secondArray // can't do this cause first is of Int type and second is of string type
 
 var finalArray = firstArray + [1,2,3,4,5]
-print(finalArray)
+//print(finalArray)
+
+//appending/deleting values
+var abc: [String] = ["a","b","c","d"]
+abc.append("e")
+//print(abc)
+abc.insert("f", at: 1)
+//print(abc)
+abc.remove(at: 1)
+//print(abc)
+
+for letter in abc {
+    //print("\(letter)")
+}
+
+for (index, letter) in abc.enumerated() {
+    print("\(index+1): \(letter)")
+}
 
 
