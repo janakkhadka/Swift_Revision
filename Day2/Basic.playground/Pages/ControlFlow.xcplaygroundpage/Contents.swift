af9567283bd20f130b183035e3e2eba2f1ae6e2f
Janak Khadka <janakkhadka08@gmail.com>
2025-02-26 22:42:39 +0545

    for-in loop

---
+++ Day2/Basic.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var dictionary = ["name": "janak", "age": "25", "address": "Dhankuta"]
+
+for (key, value) in dictionary {
+    //print("\(key): \(value)")
+}
+
+for index in 1...5 {
+    print(index)
+}
+
+
+
