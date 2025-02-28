4fbb7a2b179def37a46c5f79295ce5ef4c422061
Janak Khadka <janakkhadka08@gmail.com>
2025-02-28 22:39:08 +0545

    closures

---
+++ Day2/Basic.playground/Pages/ClosuresNEnumerations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+//closures
+var names = ["janak", "bishal", "kishor", "subash", "dhrub", "sujan", "dipesh", "kailash"]
+var sortedNames = names.sorted(by: { s1, s2 in
+    s1 < s2
+})
+print(sortedNames)
