512d2e997729619470dd9378f7477d6c2fde8eda
Janak Khadka <janakkhadka08@gmail.com>
2025-02-28 23:07:52 +0545

    closure 3

--- Day2/Basic.playground/Pages/ClosuresNEnumerations.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/ClosuresNEnumerations.xcplaygroundpage/Contents.swift
@@ -12,6 +12,17 @@ var sortedNames = names.sorted(by: { s1, s2 in
 var sortedNamesOperatorMethod = names.sorted(by: <)  //xoto tarika ho
 //print(sortedNames)
 
+func makeIncrementer(incrementAmount: Int) -> () -> Int {
+    var total = 0
+    let incrementer: () -> Int = {
+        total += incrementAmount
+        return total
+    }
+    return incrementer
+}
 
+let incrementByTwo = makeIncrementer(incrementAmount: 2)
+print(incrementByTwo()) //2 aauxa
+print(incrementByTwo()) //4 aauxa
 
 
