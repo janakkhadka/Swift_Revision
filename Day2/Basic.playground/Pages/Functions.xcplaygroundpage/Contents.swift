a0b155e219d8e8ea67f87f9508726c67e2927f60
Janak Khadka <janakkhadka08@gmail.com>
2025-03-03 23:38:33 +0545

    variadic parameters

--- Day2/Basic.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -13,4 +13,12 @@ func greet(to name: String, with greeting: String) -> String {
 func greet2(_ name: String, _ greeting: String) -> String {
     return "\(greeting), \(name)!"
 }
-print(greet2("janak", "good evening"))
+//print(greet2("janak", "good evening"))
+
+
+//variadic paramters: jati ota ni parameter pass garna milne
+func sum(_ numbers: Int...) -> Int {
+    return numbers.reduce(0, +)  //reduce is a higher order function, it reduces numbers(array) to a single value; 0 is initial sum and + will add each array elements turn by turn
+}
+//print(sum(1,2,3,4,5))
+
