cc256cf82abc7f30673d46976b13740c752fc741
Janak Khadka <janakkhadka08@gmail.com>
2025-03-03 23:32:22 +0545

    ommting argument label

--- Day2/Basic.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -7,4 +7,10 @@ func greet(to name: String, with greeting: String) -> String {
     return "\(greeting), \(name)!"
 }
 
-greet(to: "janak", with: "Good Morning!")
+//print(greet(to: "janak", with: "Good Morning"))
+
+//omiting argument label
+func greet2(_ name: String, _ greeting: String) -> String {
+    return "\(greeting), \(name)!"
+}
+print(greet2("janak", "good evening"))
