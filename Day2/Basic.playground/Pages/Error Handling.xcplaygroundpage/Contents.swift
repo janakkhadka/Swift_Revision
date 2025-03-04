c06cbfbcb383809c6d49dc7d650d91eb6393bef7
Janak Khadka <janakkhadka08@gmail.com>
2025-03-04 21:07:11 +0545

    error handling 2

--- Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -49,5 +49,18 @@ class VendingMachine {
         print("Dispensing \(name)")
     }
 }
-
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+do {
+    try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+    print("Success! Yum.")
+} catch VendingMachineError.invalidSelection {
+    print("Invalid Selection.")
+} catch VendingMachineError.outOfStock {
+    print("Out of Stock.")
+} catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+    print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+} catch {
+    print("Unexpected error: \(error).")
+}
 
