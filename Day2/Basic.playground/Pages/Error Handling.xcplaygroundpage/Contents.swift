cba876d2283310fc89cf4d3f6878d42b05caf831
Janak Khadka <janakkhadka08@gmail.com>
2025-03-05 22:25:10 +0545

    with do

--- Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -50,4 +50,13 @@ class VendingMachine {
     }
 }
 
+//do
+func eat(item: String) throws {
+    do {
+        try vendingMachine.vend(itemNamed: item)
+    } catch VendingMachineError.invalidSelection, VendingMachineError.insufficientFunds, VendingMachineError.outOfStock {
+        print("Invalid selection, out of stock, or not enough money.")
+    }
+}
+
 
