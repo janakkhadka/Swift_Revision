dfec93dfdbf790ce8d38468a8e4016eb096062a6
Janak Khadka <janakkhadka08@gmail.com>
2025-03-06 21:57:31 +0545

    eror handling

--- Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -2,84 +2,27 @@
 
 import Foundation
 
-enum VendingMachineError: Error {
-    case invalidSelection
-    case insufficientFunds(coinsNeeded: Int)
-    case outOfStock
-}
-//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
-struct Item {
-    var price: Int
-    var count: Int
+enum NetworkError: Error {
+    case badURL
+    case noInternet
+    case serverError(code: Int)
 }
 
-class VendingMachine {
-    var inventory = [
-        "Candy Bar": Item(price: 12, count: 7),
-        "Chips": Item(price: 10, count: 4),
-        "Pretzels": Item(price: 7, count: 11)
-    ]
-    var coinsDeposited = 0
-
-
-    func vend(itemNamed name: String) throws {
-        guard let item = inventory[name] else {
-            throw VendingMachineError.invalidSelection
-        }
-
-
-        guard item.count > 0 else {
-            throw VendingMachineError.outOfStock
-        }
-
-
-        guard item.price <= coinsDeposited else {
-            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
-        }
-
-
-        coinsDeposited -= item.price
-
-
-        var newItem = item
-        newItem.count -= 1
-        inventory[name] = newItem
-
-
-        print("Dispensing \(name)")
+func fetchData(from url: String) throws -> String {
+    guard url == "https://www.google.com" else {
+        throw NetworkError.badURL
     }
+    return "data loaded successfully"
 }
 
-//do
-
-enum StatisticsError: Error {
-    case noRatings
-    case invalidRating(Int)
-}
-
-func summarize(_ ratings: [Int]) throws(StatisticsError) {
-    guard !ratings.isEmpty else { throw .noRatings }
+let result = try? fetchData(from: "invalid_url")
+print(result)  // nil, just simple wala error handling
 
 
-    var counts = [1: 0, 2: 0, 3: 0]
-    for rating in ratings {
-        guard rating > 0 && rating <= 3 else { throw .invalidRating(rating) }
-        counts[rating]! += 1
-    }
 
 
-    print("*", counts[1]!, "-- **", counts[2]!, "-- ***", counts[3]!)
-}
-
-func someThrowingFunction() -> throws {
-    let ratings = [1, 2, 3, 2, 2, 1]
-    try summarize(ratings)
-}
 
 
-func someThrowingFunction() -> throws(any Error) {
-    let ratings = [1, 2, 3, 2, 2, 1]
-    try summarize(ratings)
-}
+
 
 
