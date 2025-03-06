207a43b6d2f63f34f2e6d26b210281cb53344824
Janak Khadka <janakkhadka08@gmail.com>
2025-03-06 21:57:56 +0545

    with do

--- Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -19,7 +19,16 @@ let result = try? fetchData(from: "invalid_url")
 print(result)  // nil, just simple wala error handling
 
 
-
+do {
+    let result = try fetchData(from: "invalid_url")
+    print(result)
+} catch NetworkError.badURL {
+    print("Invalid URL provided.")
+} catch NetworkError.noInternet {
+    print("No internet connection.")
+} catch let error {
+    print("An unexpected error occurred: \(error)")
+}
 
 
 
