bdf0af2ce3e7b2666fa886e31b1ef1ecb52d87c9
Janak Khadka <janakkhadka08@gmail.com>
2025-03-08 11:34:07 +0545

    document directory specify gareko

--- Day2/Basic.playground/Pages/File Handling.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/File Handling.xcplaygroundpage/Contents.swift
@@ -2,4 +2,11 @@
 
 import Foundation
 
+//document directory where file will be stored
+func getDocumentsDirectory() -> URL {
+    FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+}
+let docURL = getDocumentsDirectory()
+
+//
 
