3cae8828199afd8031a2302049beedfa28b3ac1b
Janak Khadka <janakkhadka08@gmail.com>
2025-03-11 20:01:39 +0545

    swift 5

--- Day2/Basic.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
+++ Day2/Basic.playground/Pages/Concurrency.xcplaygroundpage/Contents.swift
@@ -4,9 +4,9 @@ import Foundation
 
 //simple program to fetch data
 // async le chai function lai asynchronous banauxa
-//func fetchData() async -> String {
-//    return "Data fetched!"
-//}
+func fetchData() async -> String {
+    return "Data fetched!"
+}
 
 //await le chai asynchronous function call garna help garxa
 //Task {} creates a new concurrent task.
